d5062703e833910a757a0877bb77853112b97217
orizv <ori.zviran@gmail.com>
2016-06-08 20:34:59 +0300

    xhandler going to be changed

--- src/XHandeler.java
+++ src/XHandeler.java
@@ -6,9 +6,11 @@ public class XHandeler {
     private Point[] _currArr;
     private Point[] _addedPoints;
     private Point[] _deletedPoints;
+    private  boolean[] _isDeleted;
+    private boolean[] _addedDeleted;
     private int[] _sumPoints;
-    static int numAdded;
-    static int numDeleted;
+    private static int _numAdded;
+    private static int _numDeleted;
 
     /**
      * constructor for the X array
@@ -20,8 +22,14 @@ public class XHandeler {
         this.initialize(arr);
         _addedPoints = new Point[10 * (int) Math.log(arr.length)];
         _deletedPoints = new Point[10 * (int) Math.log(arr.length)];
-        numAdded = 0;
-        numDeleted = 0;
+        _isDeleted = new boolean[arr.length];
+        for(int i=0;i<arr.length;i++)
+            _isDeleted[i]=false;
+        _addedDeleted=new boolean[10 * (int) Math.log(arr.length];
+        for (int i=0;i<_addedDeleted.length;i++)
+            _addedDeleted[i]=true;
+        _numAdded = 0;
+        _numDeleted = 0;
     }
 
     /**
@@ -44,21 +52,83 @@ public class XHandeler {
         } else {
             _currArr = arr;
         }
+        _sumPoints=new int[n];
+        int sum=0;
+        for (int i = 0; i < n; i++) {
+            sum=sum+_currArr[i].getY();
+            _sumPoints[i]=sum;
+        }
     }
 
     public void insert(Point p) {
-
+        _addedDeleted[_numAdded]=false;
+        _addedPoints[_numAdded]=p;
+        _numAdded++;
     }
 
-    public Point extract(Point p) {
-
+    public void extract(Point p) {
+        int index[]=search(p.getX());
+        if (index[0]==1)
+            _isDeleted[index[1]]=true;
+        else if(index[0]==2)
+            _addedDeleted[index[1]]=true;
+        _deletedPoints[_numDeleted]=p;
+        _numDeleted++;
     }
 
-    public boolean search(Point p){
-
+    public int[] search(int x){
+        int ans=binSearch(x,_currArr,_currArr.length);
+        if (ans==-1) {
+            ans = linSearch(x, _addedPoints, _numAdded);
+            if (ans != -1) {
+                int[] answer = {2, ans};
+                return answer;
+            }
+            else
+                throw new RuntimeException("Fuck you it don't exist");
+        }
+        else{
+            int[] answer = {1, ans};
+            return answer;
+        }
     }
 
+    private int binSearch (int x, Point[] point, int size){
+        int first = 0;
+        int last = size;
+        int exist=0;
+        boolean found = false;
+        int mid = (last - first) / 2;
+        while(!found&exist==0) {
+            if (last <= first)
+                exist = -1;
+            if (exist == 0) {
+                if (point[mid].getX() == x) {
+                    found = true;
+                } else if (point[mid].getX() > x) {
+                    last = mid - 1;
+                } else first = mid + 1;
+            }
+        }
+        if(found)
+            return mid;
+        else
+            return -1;
+    }
 
-
+    private int linSearch (int x, Point[] point, int size){
+        boolean found=false;
+        int i;
+        for (i=0;i<size&!found;i++){
+            if(point[i].getX()==x){
+                found=true;
+                i--;
+            }
+        }
+        if (found)
+            return i;
+        else
+            return -1;
+    }
 
 }
