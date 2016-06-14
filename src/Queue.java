cbc65c4f14c929a0b065baa2286a2a83c1ccda94
Lior <liorbs6@gmail.com>
2016-06-14 11:51:01 +0300

    finished stack

--- src/Queue.java
+++ src/Queue.java
@@ -2,14 +2,51 @@
  * Created by liorbass on 14/06/2016.
  */
 public class Queue {
+    private QueueNode start;
+    private QueueNode end;
 
-    public void inQueue(Point p){
+    public Queue(){
+        start=null;
+        end=null;
+    }
+
+    /**
+     * Insert a new point to the queue
+     * @param p point to insert
+     */
+    public void enqueue(Point p){
+        if (end==null){
+            end=new QueueNode(p);
+            start=end;
+        }
+        else{
+            QueueNode t=new QueueNode(p);
+            end.set_next(t);
+            end=t;
+        }
 
     }
-    public Point DeQueue(){
 
+    /**
+     * get the first queued point from the queue
+     * @return point
+     */
+    public Point dequeue(){
+        if(start==null)
+            return null;
+        else {
+            Point ans=start.getData();
+            start=start.get_next();
+            return ans;
+        }
     }
+
+    /**
+     * check if the queue is empty
+     * @return true if there is no items in the queue
+     */
     public boolean isEmpty(){
-        
+        return (start==null&&end==null);
+
     }
 }
--- src/QueueNode.java
+++ src/QueueNode.java
@@ -2,6 +2,23 @@
  * Created by liorbass on 14/06/2016.
  */
 public class QueueNode {
-    Point p;
+    Point _this;
+    QueueNode _next;
+    public QueueNode(Point p){
+        this._this=p;
+        _next=null;
+    }
+    public void set_next(Point p){
+        _next=new QueueNode(p);
+    }
+    public void set_next(QueueNode qn){
+        _next=qn;
+    }
+    public Point getData(){
+        return _this;
+    }
+    public QueueNode get_next(){
+        return  _next;
+    }
 
 }
