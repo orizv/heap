bb67342825c99e1ba78646d83be03887bda19fbd
Lior <liorbs6@gmail.com>
2016-06-14 11:33:26 +0300

    queue start work

---
+++ src/Queue.java
@@ -0,0 +1,15 @@
+/**
+ * Created by liorbass on 14/06/2016.
+ */
+public class Queue {
+
+    public void inQueue(Point p){
+
+    }
+    public Point DeQueue(){
+
+    }
+    public boolean isEmpty(){
+        
+    }
+}
---
+++ src/QueueNode.java
@@ -0,0 +1,7 @@
+/**
+ * Created by liorbass on 14/06/2016.
+ */
+public class QueueNode {
+    Point p;
+
+}
